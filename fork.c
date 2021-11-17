2aa56352d262088c9da7c32781a32418d84542c5
Sarah Leong <sleong20@stuy.edu>
2021-11-16 20:36:23 -0500

    randomizes seconds to sleep from range of 2-5

---
+++ fork.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    // int f1 = fork();
+    // int f2 = fork();
+    srand(time(NULL));
+    int slec = 2+(rand()%4);
+    // printf("r: %d\tseconds to sleep: %d\n",r,slec);
+    // if (f1) {
+
+    // }
+    // else if (f2) {
+
+    // }
+    // else {
+
+    // }
+
+    return 0;
+}
