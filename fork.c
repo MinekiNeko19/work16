9e912285b3aac1372e0c98683320cf91823da3d3
Sarah Leong <sleong20@stuy.edu>
2021-11-16 20:51:01 -0500

    separated working with children

--- fork.c
+++ fork.c
@@ -3,20 +3,27 @@
 #include <time.h>
 
 int main() {
-    // int f1 = fork();
-    // int f2 = fork();
+    printf("Parent PID %d is running to catch its children!\n", getpid());
+
+    int status,w;
     srand(time(NULL));
     int slec = 2+(rand()%4);
-    // printf("r: %d\tseconds to sleep: %d\n",r,slec);
-    // if (f1) {
+    int f1 = fork();
+    int f2 = fork();
+    // printf("Child PID %d and Child PID %d are sleep running!\n\n", f1, f2);
 
-    // }
-    // else if (f2) {
-
-    // }
-    // else {
-
-    // }
+    // printf("seconds to sleep: %d\n",slec);
+    if (f1 && f2) {
+        if (f1) {
+            printf("child PID %d is here!\n",f1);
+        }
+        if (f2){
+            printf("child PID %d is here!\n",f2);
+        }
+    }
+    else {
+        printf("no grandchildren here...\n");
+    }
 
     return 0;
 }
