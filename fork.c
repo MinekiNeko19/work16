c4aafb514f648326c00b5adbc21dc699506a4c51
Sarah Leong <sleong20@stuy.edu>
2021-11-16 21:43:34 -0500

    forking twice gives 4 processes

--- fork.c
+++ fork.c
@@ -5,11 +5,21 @@
 int main() {
     printf("Parent PID %d prefork\n", getpid());
 
-    int status,w;
+    int status,w,f1,f2;
     srand(time(NULL));
     int slec;
-    int f1 = fork();
-    int f2 = fork();
+    f1 = fork();
+    f2 = fork();
+
+    // if (f1||f2) {
+    //     printf("still the parent %d\n", getpid());
+    //     f2 = fork();
+    //     printf("parent %d\tchild1 %d\tchild2 %d\n", getpid(), f1, f2);
+
+    // } else {
+    //     printf("child %d parent %d\n", getpid(), getppid());
+    // }
+    printf("pid: %d\tppid: %d\n", getpid(), getppid());
 
     return 0;
 }
