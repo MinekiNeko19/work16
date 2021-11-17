32e120426499349b041e18e97d11ad382ac55999
Sarah Leong <sleong20@stuy.edu>
2021-11-16 21:05:33 -0500

    why are there 3 children... at least the parent is ending alright

--- fork.c
+++ fork.c
@@ -7,22 +7,27 @@ int main() {
 
     int status,w;
     srand(time(NULL));
-    int slec = 2+(rand()%4);
+    int slec;
     int f1 = fork();
     int f2 = fork();
     // printf("Child PID %d and Child PID %d are sleep running!\n\n", f1, f2);
 
     // printf("seconds to sleep: %d\n",slec);
     if (f1 && f2) {
-        if (f1) {
-            printf("child PID %d is here!\n",f1);
-        }
-        if (f2){
-            printf("child PID %d is here!\n",f2);
-        }
+        printf("Actually... parent PID %d will wait for a child to stop\n", getpid());
+        w = wait(&status);
+        // if (f1) {
+        //     printf("child PID %d is here!\n",f1);
+        // }
+        // if (f2){
+        //     printf("child PID %d is here!\n",f2);
+        // }
+        printf("parent %d ended because a child ended\n", getpid());
     }
     else {
-        printf("no grandchildren here...\n");
+        slec = 2+(rand())%4;
+        printf("Child PID %d with parent %d is sleep running for %d seconds now\n", getpid(), getppid(), slec);
+        sleep(slec);
     }
 
     return 0;
